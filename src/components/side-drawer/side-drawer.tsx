2f2990f493ae549faa44347047242190554ae171
divineLush <homeoflucifer@gmail.com>
2020-12-05 10:33:16 -0500

    some styles and animations

--- src/components/side-drawer/side-drawer.tsx
+++ src/components/side-drawer/side-drawer.tsx
@@ -6,18 +6,21 @@ import { Component, Prop, h } from '@stencil/core';
   shadow: true,
 })
 export class SideDrawer {
-  @Prop() title: string;
+  // by default prop chenges are not reflected to the attribute
+  @Prop({ reflect: true }) title: string;
 
   render() {
     return (
       <aside class="fancy-side-drawer">
-        <header>
-            <h1>{ this.title }</h1>
+        <header class="fancy-side-drawer__header">
+            <h1 class="fancy-side-drawer__header-title">
+                { this.title }
+            </h1>
         </header>
         <main>
           <slot />
         </main>
       </aside>
-    );
+    )
   }
 }
