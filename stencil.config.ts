d8fd99a4a6ce10ea37a3b8ced8cf1a4e41ecc669
divineLush <homeoflucifer@gmail.com>
2020-12-27 03:29:36 -0500

    added comments

--- stencil.config.ts
+++ stencil.config.ts
@@ -13,9 +13,16 @@ export const config: Config = {
     {
       type: 'docs-readme',
     },
+    // www type spits out a full project which you could deploy to a static host
+    // useful when entire frontend is build from stencil components
     {
       type: 'www',
       serviceWorker: null, // disable service workers
     },
   ],
+  // bundles is not added by default
+  // allows to define how components are bundled together
+  // dependencies analysis etc.
+  // allows to load components only where they are attached to DOM
+  // bundles: []
 };
